475ef3c8c58a008f4e465970bc1bb11e20555478
Makotox0207 <xiaoxishicheng@gmail.com>
2021-07-23 01:27:37 +0900

    events update

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -17,6 +17,16 @@ class EventsController < ApplicationController
       render json: event.errors, status: 422
     end
   end
+
+  def update
+    event = Event.find(params[:id])
+    if event.update(event_params)
+      render json: event
+    else
+      render json: event.errors, status: 422
+    end
+  end
+
   private 
   def event_params
     params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color)
