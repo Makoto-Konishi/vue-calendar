a199d14a11a69fbfd4fbb14df7329692128c1e31
Makotox0207 <xiaoxishicheng@gmail.com>
2021-07-23 01:15:15 +0900

    event createアクション

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -6,4 +6,19 @@ class EventsController < ApplicationController
   def show
     render json: Event.find(params[:id])
   end
+
+  def create
+    event = Event.new(event_params)
+    if event.save
+      # 保存したデータをjsonに変換してクライアントに返す
+      render json: event
+    else
+      # 422 サーバ側でリクエストを正しく処理できなかった時に返すステータスコード
+      render json: event.errors, status: 422
+    end
+  end
+  private 
+  def event_params
+    params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color)
+  end
 end
