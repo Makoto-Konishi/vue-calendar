851a5b4deef1c6996d5b850881b275fd615b6bd1
Makotox0207 <xiaoxishicheng@gmail.com>
2021-08-01 01:36:52 +0900

    日付選択フォーム

--- frontend/src/components/ EventFormDialog.vue
+++ frontend/src/components/ EventFormDialog.vue
@@ -9,6 +9,10 @@
       <DialogSection icon="mdi-square" :color="event.color || 'blue'">
         <v-text-field v-model="name" label="タイトル"></v-text-field>
       </DialogSection>
+      <DialogSection icon="mdi-clock-outline">
+        <DateForm v-model="startDate" />
+        <DateForm v-model="endDate" />
+      </DialogSection>
     </v-card-text>
     <v-card-actions class="d-flex justify-end">
       <v-btn @click="submit">保存</v-btn>
@@ -18,18 +22,28 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import DialogSection from './DialogSection';
+import DialogSection from './DialogSection.vue';
+import DateForm from './DateForm.vue';
+import { format } from 'date-fns';
 
 export default {
   name: 'EventFormDialog',
   components: {
     DialogSection,
+    DateForm,
   },
   data: () => ({
     name: '',
+    startDate: null,
+    endDate: null,
   }),
   computed: {
     ...mapGetters('events', ['event']),
+  },
+  created() {
+    // dataで初期化したstartDate変数に、カレンダーでクリックした時の日付を文字列に変換して代入
+    this.startDate = format(this.event.start, 'yyyy/MM/dd');
+    this.endDate = format(this.event.end, 'yyyy/MM/dd');
   },
   methods: {
     ...mapActions('events', ['setEventActions', 'setEditMode', 'createEvent']),
@@ -40,12 +54,12 @@ export default {
     submit() {
       const params = {
         name: this.name,
-        start: this.event.start,
-        end: this.event.end,
+        start: this.startDate,
+        end: this.endDate,
       };
       this.createEvent(params);
       this.closeDialog();
-    }
+    },
   },
 };
 </script>
